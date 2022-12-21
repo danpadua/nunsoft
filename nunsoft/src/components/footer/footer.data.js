f0ecd6794eba70fc9d372298c1a93e544e94ca52
Danilo de Pádua <danilo.padua@pilla.tech>
2022-12-20 23:47:43 -0300

    Novo layout

---
+++ nunsoft/src/components/footer/footer.data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    header: 'Sobre nós',
+    items: [
+      {
+        path: '/',
+        label: 'About Us',
+      }
+    ],
+  },
+  {
+    header: 'Informações',
+    items: [
+      {
+        path: '/',
+        label: 'Política de privacidade',
+      },
+      {
+        path: '/',
+        label: 'Termos & Condições',
+      },
+    ],
+  },
+  {
+    header: 'Política de Privacidade',
+    items: [
+      {
+        path: '/',
+        label: 'Segurança',
+      },
+      {
+        path: '/',
+        label: 'LGPD',
+      },
+    ],
+  },
+];
---
+++ nunsoft/src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Início',
+  },
+  {
+    path: 'feature',
+    label: 'Serviços',
+  },
+  {
+    path: 'testimonial',
+    label: 'Clientes',
+  },
+  // {
+  //   path: 'pricing',
+  //   label: 'Planos',
+  // },
+];
